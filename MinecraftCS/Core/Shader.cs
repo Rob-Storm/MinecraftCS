c1c723b03002f45d55887533e42d18054b02602a
The1Wolfcast <the1wolfcast@gmail.com>
2024-03-17 19:16:20 -0500

    added shader class

---
+++ MinecraftCS/Core/Shader.cs
@@ -0,0 +1,93 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace MinecraftCS.Core
+{
+    public class Shader : IDisposable
+    {
+        int Handle;
+        private bool disposedValue = false;
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            int VertexShader, FragmentShader;
+
+            string VertexShaderSource = File.ReadAllText(vertexPath);
+
+            string FragmentShaderSource = File.ReadAllText(fragmentPath);
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(VertexShader);
+                Console.Write(infoLog);
+            }
+
+            GL.CompileShader(FragmentShader);
+
+            GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out int successFrag);
+            if (successFrag == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(FragmentShader);
+                Console.Write(infoLog);
+            }
+
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out int successProgram);
+
+            if (successProgram == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(Handle);
+                Console.WriteLine(infoLog);
+            }
+
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(VertexShader);
+            GL.DeleteShader(FragmentShader);
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~Shader()
+        {
+            if(!disposedValue)
+            {
+                Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+    }
+}
---
+++ MinecraftCS/Game.cs
@@ -0,0 +1,66 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using MinecraftCS.Core;
+
+namespace MinecraftCS
+{
+    public class Game : GameWindow
+    {
+        #region Properties and Fields
+
+        public float[] vertices = 
+            {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f, 
+                0.0f,  0.5f, 0.0f  
+            };
+
+        public int VertexBufferObject;
+
+        public Shader shader;
+
+        #endregion
+
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
+        {
+            
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if(KeyboardState.IsKeyDown(Keys.Escape)) 
+            {
+                Close();
+            }
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            SwapBuffers();
+
+            VertexBufferObject = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+    }
+}
